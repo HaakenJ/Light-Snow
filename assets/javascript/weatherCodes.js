aed3afc43e8efade9733f32314193d528aa174cc
Haakasaurus <kramerhjohnson@gmail.com>
2019-10-08 21:29:13 -0700

    Got the light and light.js is now fully functional.

---
+++ assets/javascript/weatherCodes.js
@@ -0,0 +1,460 @@
+let weatherCodes = {
+    2: {
+        200: {
+            'params': {
+                'color': 'purple',
+                'brightness': '0.4'
+            },
+            'desc': 'thunderstorm with light rain',
+
+
+        },
+        201: {
+            'params': {
+                'color': 'yellow',
+                'brightness': '0.7'
+            },
+            'desc': 'thunderstorm with rain',
+
+
+        },
+        202: {
+            'params': {
+                'color': 'red',
+                'brightness': '0.9'
+            },
+            'desc': 'thunderstorm with heavy rain',
+
+        },
+        210: {
+            'params': {
+                'color': 'yellow',
+                'brightness': '0.5'
+            },
+            'desc': 'light thunderstorm',
+
+        },
+        211: {
+            'params': {
+                'color': 'yellow',
+                'brightness': '0.8'
+            },
+            'desc': 'thunderstorm',
+
+        },
+        212: {
+            'params': {
+                'color': 'red',
+                'brightness': '1.0'
+            },
+            'desc': 'heavy thunderstorm',
+
+        },
+        221: {
+            'params': {
+                'color': 'yellow',
+                'brightness': '0.6'
+            },
+            'desc': 'ragged thunderstorm',
+
+        },
+        230: {
+            'params': {
+                'color': 'yellow',
+                'brightness': '0.1'
+            },
+            'desc': 'thunderstorm with light drizzle',
+
+        },
+        231: {
+            'params': {
+                'color': 'yellow',
+                'brightness': '0.2'
+            },
+            'desc': 'thunderstorm with drizzle',
+
+        },
+        232: {
+            'params': {
+                'color': 'yellow',
+                'brightness': '0.3'
+            },
+            'desc': 'thunderstorm with heavy drizzle',
+
+        }
+    },
+    3: {
+        300: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '0.2'
+            },
+            'desc': 'light intensity drizzle',
+
+        },
+        301: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '0.4'
+            },
+            'desc': 'drizzle',
+
+        },
+        302: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '0.5'
+            },
+            'desc': 'heavy intensity drizzle',
+
+        },
+        310: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '0.3'
+            },
+            'desc': 'light intensity drizzle rain',
+
+        },
+        311: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '0.6'
+            },
+            'desc': 'drizzle rain',
+
+        },
+        312: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '0.7'
+            },
+            'desc': 'heavy intensity drizzle rain',
+
+        },
+        313: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '0.8'
+            },
+            'desc': 'shower rain and drizzle',
+
+        },
+        314: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '1.0'
+            },
+            'desc': 'heavy shower rain and drizzle',
+
+        },
+        321: {
+            'params': {
+                'color': 'cyan',
+                'brightness': '0.9'
+            },
+            'desc': 'shower drizzle',
+
+        }
+    },
+    5: {
+        500: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.5'
+            },
+            'desc': 'light rain',
+
+        },
+        501: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.6'
+            },
+            'desc': 'moderate rain',
+
+        },
+        502: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.7'
+            },
+            'desc': 'heavy intensity rain',
+
+        },
+        503: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.9'
+            },
+            'desc': 'very heavy rain',
+
+        },
+        504: {
+            'params': {
+                'color': 'blue',
+                'brightness': '1.0'
+            },
+            'desc': 'extreme rain',
+
+        },
+        511: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.8'
+            },
+            'desc': 'freezing rain',
+
+        },
+        520: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.1'
+            },
+            'desc': 'light intensity shower rain',
+
+        },
+        521: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.2'
+            },
+            'desc': 'shower rain',
+
+        },
+        522: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.3'
+            },
+            'desc': 'heavy intensity shower rain',
+
+        },
+        531: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.4'
+            },
+            'desc': 'ragged shower rain',
+
+        }
+    },
+    6: {
+        600: {
+            'params': {
+                'color': 'green',
+                'brightness': '0.8'
+            },
+            'desc': 'light snow',
+
+        },
+        601: {
+            'params': {
+                'color': 'green',
+                'brightness': '0.9'
+            },
+            'desc': 'snow',
+
+        },
+        602: {
+            'params': {
+                'color': 'green',
+                'brightness': '1.0'
+            },
+            'desc': 'heavy snow',
+
+        },
+        611: {
+            'params': {
+                'color': 'purple',
+                'brightness': '0.7'
+            },
+            'desc': 'sleet',
+
+        },
+        612: {
+            'params': {
+                'color': 'purple',
+                'brightness': '0.6'
+            },
+            'desc': 'light shower sleet',
+
+        },
+        613: {
+            'params': {
+                'color': 'purple',
+                'brightness': '0.5'
+            },
+            'desc': 'shower sleet',
+
+        },
+        615: {
+            'params': {
+                'color': 'purple',
+                'brightness': '0.4'
+            },
+            'desc': 'light rain and snow',
+
+        },
+        616: {
+            'params': {
+                'color': 'purple',
+                'brightness': '0.3'
+            },
+            'desc': 'rain and snow',
+
+        },
+        620: {
+            'params': {
+                'color': 'green',
+                'brightness': '0.5'
+            },
+            'desc': 'light shower snow',
+
+        },
+        621: {
+            'params': {
+                'color': 'green',
+                'brightness': '0.6'
+            },
+            'desc': 'shower snow',
+
+        },
+        622: {
+            'params': {
+                'color': 'green',
+                'brightness': '0.7'
+            },
+            'desc': 'heavy shower snow',
+
+        }
+    },
+    7: {
+        701: {
+            'params': {
+                'color': 'pink',
+                'brightness': '0.2'
+            },
+            'desc': 'mist',
+
+        },
+        711: {
+            'params': {
+                'color': 'orange',
+                'brightness': '0.4'
+            },
+            'desc': 'smoke',
+
+        },
+        721: {
+            'params': {
+                'color': 'pink',
+                'brightness': '0.3'
+            },
+            'desc': 'haze',
+
+        },
+        731: {
+            'params': {
+                'color': 'orange',
+                'brightness': '0.4'
+            },
+            'desc': 'sand/ dust whirls',
+
+        },
+        741: {
+            'params': {
+                'color': 'pink',
+                'brightness': '0.4'
+            },
+            'desc': 'fog',
+
+        },
+        751: {
+            'params': {
+                'color': 'orange',
+                'brightness': '0.3'
+            },
+            'desc': 'sand',
+
+        },
+        761: {
+            'params': {
+                'color': 'orange',
+                'brightness': '0.7'
+            },
+            'desc': 'dust',
+
+        },
+        762: {
+            'params': {
+                'color': 'orange',
+                'brightness': '0.8'
+            },
+            'desc': 'volcanic ash',
+
+        },
+        771: {
+            'params': {
+                'color': 'orange',
+                'brightness': '0.6'
+            },
+            'desc': 'squalls',
+
+        },
+        781: {
+            'params': {
+                'color': 'red',
+                'brightness': '1.0'
+            },
+            'desc': 'tornado',
+
+        }
+    },
+    8: {
+        800: {
+            'params': {
+                'color': 'white',
+                'brightness': '1.0',
+                'kelvin': '2700'
+            },
+            'desc': 'clear sky',
+
+            
+        },
+        801: {
+            'params': {
+                'color': 'white',
+                'brightness': '0.5',
+                'kelvin': '2700'
+            },
+            'desc': 'few clouds: 11-25%',
+
+            
+        },
+        802: {
+            'params': {
+                'color': 'white',
+                'brightness': '0.3',
+                'kelvin': '2700'
+            },
+            'desc': 'scattered clouds: 25-50%',
+
+            
+        },
+        803: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.4',
+            },
+            'desc': 'broken clouds: 51-84%',
+        },
+        804: {
+            'params': {
+                'color': 'blue',
+                'brightness': '0.2',
+            },
+            'desc': 'overcast clouds: 85-100%',
+        }
+    }
+};
