68ea5b5f9004cef3eec256b802aa688675cdbbae
Haakasaurus <kramerhjohnson@gmail.com>
2019-10-10 13:23:33 -0700

    Made an intro page with animation.

---
+++ assets/javascript/database.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBQJtNCS5HMLu4y7RCp5Og7D3DBDzh9O18",
+    authDomain: "lightsnow-32ff9.firebaseapp.com",
+    databaseURL: "https://lightsnow-32ff9.firebaseio.com",
+    projectId: "lightsnow-32ff9",
+    storageBucket: "lightsnow-32ff9.appspot.com",
+    messagingSenderId: "253885354798",
+    appId: "1:253885354798:web:d8b109821feaf43e5560bf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
--- assets/javascript/weatherCodes.js
+++ assets/javascript/weatherCodes.js
@@ -2,8 +2,8 @@ let weatherCodes = {
     2: {
         200: {
             'params': {
-                'power': 'on',
-                'hue': 290
+                'color': 'purple',
+                'brightness': '0.4'
             },
             'desc': 'thunderstorm with light rain',
 
@@ -11,85 +11,73 @@ let weatherCodes = {
         },
         201: {
             'params': {
-                // 'power': 'on',
-                // 'hue': 280
                 'color': 'yellow',
-                'brightness': 0.7
+                'brisghtness': '0.7'
             },
             'desc': 'thunderstorm with rain',
+
+
         },
         202: {
             'params': {
-                'power': 'on',
-                'hue': 270
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 270,
-                'period': 1
+                'color': 'red',
+                'brightness': '0.9'
             },
             'desc': 'thunderstorm with heavy rain',
 
         },
         210: {
             'params': {
-                'power': 'on',
-                'hue': 300
+                'color': 'yellow',
+                'brightness': '0.5'
             },
             'desc': 'light thunderstorm',
 
         },
         211: {
             'params': {
-                'power': 'on',
-                'hue': 320
+                'color': 'yellow',
+                'brightness': '0.8'
             },
             'desc': 'thunderstorm',
 
         },
         212: {
             'params': {
-                'power': 'on',
-                'hue': 340
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 340,
-                'period': 0.5
+                'color': 'red',
+                'brightness': '1.0'
             },
             'desc': 'heavy thunderstorm',
 
         },
         221: {
             'params': {
-                'power': 'on',
-                'hue': 310
+                'color': 'yellow',
+                'brightness': '0.6'
             },
             'desc': 'ragged thunderstorm',
 
         },
         230: {
             'params': {
-                'power': 'on',
-                'hue': 290
+                'color': 'yellow',
+                'brightness': '0.1'
             },
             'desc': 'thunderstorm with light drizzle',
 
         },
         231: {
             'params': {
-                'power': 'on',
-                'hue': 285
+                'color': 'yellow',
+                'brightness': '0.2'
             },
             'desc': 'thunderstorm with drizzle',
 
         },
         232: {
             'params': {
-                'power': 'on',
-                'hue': 280
+                'color': 'yellow',
+                'brightness': '0.3'
             },
             'desc': 'thunderstorm with heavy drizzle',
 
@@ -98,69 +86,72 @@ let weatherCodes = {
     3: {
         300: {
             'params': {
-                'power': 'on',
-                'hue': 190
+                'color': 'cyan',
+                'brightness': '0.2'
             },
             'desc': 'light intensity drizzle',
+
         },
         301: {
             'params': {
-                'power': 'on',
-                'hue': 200
+                'color': 'cyan',
+                'brightness': '0.4'
             },
             'desc': 'drizzle',
+
         },
         302: {
             'params': {
-                'power': 'on',
-                'hue': 210
+                'color': 'cyan',
+                'brightness': '0.5'
             },
             'desc': 'heavy intensity drizzle',
+
         },
         310: {
             'params': {
-                'power': 'on',
-                'hue': 0.5
+                'color': 'cyan',
+                'brightness': '0.3'
             },
             'desc': 'light intensity drizzle rain',
 
         },
         311: {
             'params': {
-                'power': 'on',
-                'hue': 220
+                'color': 'cyan',
+                'brightness': '0.6'
             },
             'desc': 'drizzle rain',
 
         },
         312: {
             'params': {
-                'power': 'on',
-                'hue': 220
+                'color': 'cyan',
+                'brightness': '0.7'
             },
             'desc': 'heavy intensity drizzle rain',
 
         },
         313: {
             'params': {
-                'power': 'on',
-                'hue': 220
+                'color': 'cyan',
+                'brightness': '0.8'
             },
             'desc': 'shower rain and drizzle',
 
         },
         314: {
             'params': {
-                'power': 'on',
-                'hue': 225
+                'color': 'cyan',
+                'brightness': '1.0'
             },
             'desc': 'heavy shower rain and drizzle',
 
         },
         321: {
             'params': {
-                'power': 'on',
-                'hue': 220
+                'color': 'cyan',
+                'brightness': '0.9'
             },
             'desc': 'shower drizzle',
 
@@ -169,92 +160,80 @@ let weatherCodes = {
     5: {
         500: {
             'params': {
-                'power': 'on',
-                'hue': 230
+                'color': 'blue',
+                'brightness': '0.5'
             },
             'desc': 'light rain',
 
         },
         501: {
             'params': {
-                'power': 'on',
-                'hue': 235
+                'color': 'blue',
+                'brightness': '0.6'
             },
             'desc': 'moderate rain',
 
         },
         502: {
             'params': {
-                'power': 'on',
-                'hue': 240
+                'color': 'blue',
+                'brightness': '0.7'
             },
             'desc': 'heavy intensity rain',
 
         },
         503: {
             'params': {
-                'power': 'on',
-                'hue': 250
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 250,
-                'period': 1
+                'color': 'blue',
+                'brightness': '0.9'
             },
             'desc': 'very heavy rain',
 
         },
         504: {
             'params': {
-                'power': 'on',
-                'hue': 260
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 260,
-                'period': 0.5
+                'color': 'blue',
+                'brightness': '1.0'
             },
             'desc': 'extreme rain',
 
         },
         511: {
             'params': {
-                'power': 'on',
-                'hue': 200
+                'color': 'blue',
+                'brightness': '0.8'
             },
             'desc': 'freezing rain',
 
         },
         520: {
             'params': {
-                'power': 'on',
-                'hue': 220
+                'color': 'blue',
+                'brightness': '0.1'
             },
             'desc': 'light intensity shower rain',
 
         },
         521: {
             'params': {
-                'power': 'on',
-                'hue': 220
+                'color': 'blue',
+                'brightness': '0.2'
             },
             'desc': 'shower rain',
 
         },
         522: {
             'params': {
-                'power': 'on',
-                'hue': 230
+                'color': 'blue',
+                'brightness': '0.3'
             },
             'desc': 'heavy intensity shower rain',
 
         },
         531: {
             'params': {
-                'power': 'on',
-                'hue': 220
+                'color': 'blue',
+                'brightness': '0.4'
             },
             'desc': 'ragged shower rain',
 
@@ -263,100 +242,88 @@ let weatherCodes = {
     6: {
         600: {
             'params': {
-                'power': 'on',
-                'hue': 100
+                'color': 'green',
+                'brightness': '0.8'
             },
             'desc': 'light snow',
 
         },
         601: {
             'params': {
-                'power': 'on',
-                'hue': 110
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 110,
-                'period': 1
+                'color': 'green',
+                'brightness': '0.9'
             },
             'desc': 'snow',
 
         },
         602: {
             'params': {
-                'power': 'on',
-                'hue': 120
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 120,
-                'period': 0.5
+                'color': 'green',
+                'brightness': '1.0'
             },
             'desc': 'heavy snow',
 
         },
         611: {
             'params': {
-                'power': 'on',
-                'hue': 170
+                'color': 'purple',
+                'brightness': '0.7'
             },
             'desc': 'sleet',
 
         },
         612: {
             'params': {
-                'power': 'on',
-                'hue': 170
+                'color': 'purple',
+                'brightness': '0.6'
             },
             'desc': 'light shower sleet',
 
         },
         613: {
             'params': {
-                'power': 'on',
-                'hue': 170
+                'color': 'purple',
+                'brightness': '0.5'
             },
             'desc': 'shower sleet',
 
         },
         615: {
             'params': {
-                'power': 'on',
-                'hue': 160
+                'color': 'purple',
+                'brightness': '0.4'
             },
             'desc': 'light rain and snow',
 
         },
         616: {
             'params': {
-                'power': 'on',
-                'hue': 150
+                'color': 'purple',
+                'brightness': '0.3'
             },
             'desc': 'rain and snow',
 
         },
         620: {
             'params': {
-                'power': 'on',
-                'hue': 100
+                'color': 'green',
+                'brightness': '0.5'
             },
             'desc': 'light shower snow',
 
         },
         621: {
             'params': {
-                'power': 'on',
-                'hue': 0.5
+                'color': 'green',
+                'brightness': '0.6'
             },
             'desc': 'shower snow',
 
         },
         622: {
             'params': {
-                'power': 'on',
-                'hue': 110
+                'color': 'green',
+                'brightness': '0.7'
             },
             'desc': 'heavy shower snow',
 
@@ -365,147 +332,127 @@ let weatherCodes = {
     7: {
         701: {
             'params': {
-                'power': 'on',
-                'hue': 180,
-                'brightness': 0.5
+                'color': 'pink',
+                'brightness': '0.2'
             },
             'desc': 'mist',
 
         },
         711: {
             'params': {
-                'power': 'on',
-                'hue': 50,
-                'brightness': 0.5
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 50,
-                'period': 1
+                'color': 'orange',
+                'brightness': '0.4'
             },
             'desc': 'smoke',
 
         },
         721: {
             'params': {
-                'power': 'on',
-                'hue': 40,
-                'brightness': 0.5
+                'color': 'pink',
+                'brightness': '0.3'
             },
             'desc': 'haze',
+
         },
         731: {
             'params': {
-                'power': 'on',
-                'hue': 30,
-                'brightness': 0.7
+                'color': 'orange',
+                'brightness': '0.4'
             },
             'desc': 'sand/ dust whirls',
+
         },
         741: {
             'params': {
-                'power': 'on',
-                'hue': 180,
-                'brightness': 0.5
+                'color': 'pink',
+                'brightness': '0.4'
             },
             'desc': 'fog',
+
         },
         751: {
             'params': {
-                'power': 'on',
-                'hue': 40,
-                'brightness': 0.6
+                'color': 'orange',
+                'brightness': '0.3'
             },
             'desc': 'sand',
+
         },
         761: {
             'params': {
-                'power': 'on',
-                'hue': 30
+                'color': 'orange',
+                'brightness': '0.7'
             },
             'desc': 'dust',
+
         },
         762: {
             'params': {
-                'power': 'on',
-                'hue': 20
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 20,
-                'period': 0.5
+                'color': 'orange',
+                'brightness': '0.8'
             },
             'desc': 'volcanic ash',
+
         },
         771: {
             'params': {
-                'power': 'on',
-                'hue': 20
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 20,
-                'period': 0.5
+                'color': 'orange',
+                'brightness': '0.6'
             },
             'desc': 'squalls',
 
         },
         781: {
             'params': {
-                'power': 'on',
-                'hue': 0,
-                'brightness': 1.0
-            },
-            'effectUrl': 'https://api.lifx.com/v1/lights/all/effects/pulse',
-            'effectParams': {
-                'power_on': true,
-                'hue': 0,
-                'period': 0.2
+                'color': 'red',
+                'brightness': '1.0'
             },
             'desc': 'tornado',
+
         }
     },
     8: {
         800: {
             'params': {
                 'color': 'white',
-                'brightness': 1.0,
-                'kelvin': 5000
+                'brightness': '1.0',
+                'kelvin': '2700'
             },
-            'desc': 'clear sky'
+            'desc': 'clear sky',
+
+
         },
         801: {
             'params': {
                 'color': 'white',
-                'brightness': 0.7,
-                'kelvin': 4000
+                'brightness': '0.5',
+                'kelvin': '2700'
             },
-            'desc': 'few clouds: 11-25%'     
+            'desc': 'few clouds: 11-25%',
+
+
         },
         802: {
             'params': {
                 'color': 'white',
-                'brightness': 0.5,
-                'kelvin': 3000
+                'brightness': '0.3',
+                'kelvin': '2700'
             },
-            'desc': 'scattered clouds: 25-50%'    
+            'desc': 'scattered clouds: 25-50%',
+
+
         },
         803: {
             'params': {
-                'color': 'white',
-                'brightness': 0.4,
-                'kelvin': 2500
+                'color': 'blue',
+                'brightness': '0.4',
             },
             'desc': 'broken clouds: 51-84%',
         },
         804: {
             'params': {
-                'color': 'white',
-                'brightness': 0.2,
-                'kelvin': 2000
+                'color': 'blue',
+                'brightness': '0.2',
             },
             'desc': 'overcast clouds: 85-100%',
         }
