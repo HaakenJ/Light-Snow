d59c7da0f9a9e7b84a097efc1ffb4732d209aed2
cprybell <cprybell@sbcglobal.net>
2019-10-10 17:49:54 -0700

    Added a file that has an object with the lat and long of all the ski resorts on the unlimited epic pass as well as a few local resorts to the Seattle area. This is the only change do not merge anything else

---
+++ assets/javascript/skiResorts.js
@@ -0,0 +1,174 @@
+let skiResorts = {
+    vail: {
+        lat: "39.6403",
+        long: "106.3742"
+    },
+    whistler: {
+        lat: "50.1150",
+        long: "122.9486"
+    },
+    northstar: {
+        lat: "39.2746",
+        long: "120.1211"
+    },
+    beavercreek: {
+        lat: "39.6042",
+        long: "106.5165"
+    },
+    breckenridge: {
+        lat: "39.4817",
+        long: "106.0384"
+    },
+    parkcity: {
+        lat: "40.6461",
+        long: "111.4980"
+    },
+    parkcity: {
+        lat: "40.6461",
+        long: "111.4980"
+    },
+    keystone: {
+        lat: "39.5792",
+        long: "105.9347"
+    },
+    heavenly: {
+        lat: "38.9353",
+        long: "119.9400"
+    },
+    kirkwood: {
+        lat: "38.7023",
+        long: "120.0722"
+    },
+    stowe: {
+        lat: "44.4654",
+        long: "72.6874"
+    },
+    wilmot: {
+        lat: "42.5130",
+        long: "88.1825"
+    },
+    aftonalps: {
+        lat: "44.8577",
+        long: "92.7878"
+    },
+    mountbrighton: {
+        lat: "42.3217",
+        long: "83.4825"
+    },
+    perisher: {
+        lat: "36.4059",
+        long: "148.4117"
+    },
+    fallscreek: {
+        lat: "36.5154",
+        long: "147.1623"
+    },
+    hotham: {
+        lat: "36.5818",
+        long: "147.4362"
+    },
+    okemo: {
+        lat: "43.4018",
+        long: "72.7166"
+    },
+    mountsunapee: {
+        lat: "43.3136",
+        long: "72.0742"
+    },
+    crestedbutte: {
+        lat: "38.8697",
+        long: "106.9878"
+    },
+    stevenspass: {
+        lat: "47.7448",
+        long: "121.0890"
+    },
+    mountsnow: {
+        lat: "42.9602",
+        long: "72.9204"
+    },
+    attitashmountainresort: {
+        lat: "44.0828",
+        long: "71.2294"
+    },
+    wildcatmountain: {
+        lat: "44.2590",
+        long: "71.2015"
+    },
+    crotchedmountain: {
+        lat: "42.9984",
+        long: "71.8737"
+    },
+    huntermountain: {
+        lat: "42.1779",
+        long: "74.2304"
+    },
+    libertymountain: {
+        lat: "39.7637",
+        long: "77.3755"
+    },
+    libertymountain: {
+        lat: "39.7637",
+        long: "77.3755"
+    },
+    roundtopmountain: {
+        lat: "40.10601",
+        long: "76.9264"
+    },
+    whitetail: {
+        lat: "39.4430",
+        long: "77.5558"
+    },
+    jackfrost: {
+        lat: "41.1100",
+        long: "75.6512"
+    },
+    bigboulder: {
+        lat: "41.1100",
+        long: "75.6512"
+    },
+    alpinevalley: {
+        lat: "41.5303",
+        long: "81.2662"
+    },
+    bostonmills: {
+        lat: "38.6968",
+        long: "76.8477"
+    },
+    madriver: {
+        lat: "40.1858",
+        long: "83.4050"
+    },
+    madriver: {
+        lat: "40.1858",
+        long: "83.4050"
+    },
+    hiddenvalley: {
+        lat: "45.3561",
+        long: "79.1297"
+    },
+    snowcreek: {
+        lat: "39.4673",
+        long: "94.9707"
+    },
+    paolipeaks: {
+        lat: "38.3328",
+        long: "86.3037"
+    },
+    mountbaker: {
+        lat: "48.8573",
+        long: "121.6667"
+    },
+    crystalmountain: {
+        lat: "46.56",
+        long: "121.29"
+    },
+    snoqualmie: {
+        lat: "47.4204",
+        long: "121.4211"
+    },
+    mounthood: {
+        lat: "45.3318",
+        long: "121.6652"
+    },
+}
