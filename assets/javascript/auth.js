37d5098ae799bae6be621483aad379ec7e4427c8
Haakasaurus <kramerhjohnson@gmail.com>
2019-10-18 10:02:45 -0700

    Added google auth and fixed some responsiveness issues with navbar.

---
+++ assets/javascript/auth.js
@@ -0,0 +1,54 @@
+const auth = firebase.auth(),
+    user = firebase.auth().currentUser,
+    provider = new firebase.auth.GoogleAuthProvider();
+
+$(document).ready(() => {
+    if (user) {
+        $('#add-resort-button').show();
+        $('#username').show();
+        $('#sign-out-submit').show();
+        let name = user.displayName;
+        $('#username').text(name);
+    } else {
+        $('.add-resort-button').hide();
+        $('#username').hide();
+        $('#sign-out-submit').hide();
+        $('#login-submit').show();
+    }
+})
+
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        $('.add-resort-button').show();
+        $('#username').show();
+        $('#sign-out-submit').show();
+        $('#login-submit').hide();
+        let name = user.displayName;
+        $('#username').text(name);
+    } else {
+        $('.add-resort-button').hide();
+        $('#username').hide();
+        $('#sign-out-submit').hide();
+        $('#login-submit').show();
+    }
+})
+
+$('#login-submit').on('click', (event) => {
+    event.preventDefault();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        console.log(result);
+    }).catch(function (error) {
+        console.log(error);
+    });
+})
+
+$('#sign-out-submit').on('click', (event) => {
+    event.preventDefault();
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+        console.log('sign out successful.')
+    }, function (error) {
+        // An error happened.
+        console.log(error);
+    });
+})
